4e4e49715141d08ff98f339881b3bb3bb3dd90fa
Damon McGraw <damon@Mephisto.local>
2013-05-29 00:20:06 -0400

    Breaking out handlers into separate methods

--- restapi.py
+++ restapi.py
@@ -1,15 +1,33 @@
-#!/usr/bin/python
-from bottle import Bottle, run, request, template
+from bottle import Bottle, run, request, response, template
 
+class baseHandler(object):
+	def handler(self, request):
+		return False
+	
+	def updateResponse(self, response):
+		return 
+
+class logHandler(baseHandler):
+	def handle(self, request):
+		with open('log', 'a') as f:	
+			f.write('{0}\t'.format(request.path))
+			for name, value in request.forms.iteritems():
+				f.write('{0}={1}&'.format(name, value.replace('\r\n', '')))
+			f.write('\n\n')
+		return super(logHandler, self).handle(request)
+
+	def updateResponse(self, response):
+		return super(logHandler, self).updateResponse(response)
+		
+		
 app = Bottle()
+handlers = [logHandler()]
 
 @app.route('/<request_path:re:.*>', method='ANY')
 def index(request_path='unknown'):
-	with open('log', 'a') as f:	
-		f.write('{0}\t'.format(request_path))
-		for name, value in request.forms.iteritems():
-			f.write('{0}={1}&'.format(name, value.replace("\r\n", "")))
-		f.write('\n\n')
-	return ''
+	for h in handlers:
+		if h.handle(request):
+			h.updateResponse(response)
 
 run(app, host='localhost', port=8080, debug=True)
+
